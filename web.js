c166741dff1139100913568db071e51dad3dad65
Ubuntu <ubuntu@ip-172-31-15-224.us-west-2.compute.internal>
2013-07-10 23:26:59 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     // response.send('Hello World 2!');
 
-	var fs = IMPORTS.require('fs')
+	var fs = require('fs');
 	var buffer = new Buffer(fs.readFileSync("index.html"));
 	response.send(buffer.toString());
 });
